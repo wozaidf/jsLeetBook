f4e383775c85f467deffd13e10922e457ed5333a
wozaidf <2217957466@qq.com>
2022-07-22 19:09:50 +0800

    first commit

---
+++ text.js
@@ -0,0 +1,24 @@
+// //1.求两数之和          双循环
+// var twoSum = function(nums, target) {
+//     for(let i =0 ; i<nums.length;i++){
+//         for(let a = i+1; a<nums.length;a++)
+//         if(nums[i]+nums[a]===target){
+//             return [a,i]
+//         }
+//     }
+    
+
+// }
+// const  c =twoSum([2,3,1],4)
+// console.log(c)
+
+
+//一次循环
+var twoSum = function(nums, target) {
+    const map1=new Map()
+    for(let i =0;i<nums.length;i++){
+        if(map1.values===target-nums[i]){
+            
+        }
+    }
+}
